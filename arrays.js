9b077875933760f58e71fa99f43b225baad473f6
Rick Battipede <rbattipede@gmail.com>
2019-05-17 02:48:55 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+function addElementToBeginningOfArray(array, element){
+  array =  [element, ...array]
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element){
+  array =  [...array, element]
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element);
+}
+
+function accessElementInArray(array,number){
+  return array[number];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray (array){
+  return array.pop();
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0,array.length-1)
+}
